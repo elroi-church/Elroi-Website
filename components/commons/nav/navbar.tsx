c00909b5908bb7b8e38667e0c4bc7cd03f8206e7
antonius joshua <antoniusjoshua47@gmail.com>
2021-12-12 17:03:11 +0700

    feat: initialize project structure

---
+++ components/commons/nav/navbar.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+// Navbar tailwind component
+const Navbar: FunctionComponent = () => {
+  return <div></div>;
+};
+
+// Tailwind Navbar Component
+export default Navbar;
