96c6c5b6ae4afd60fd8571ca36143b3184e1a151
keva damar galih <kevadamarg@gmail.com>
2022-02-19 18:44:46 +0700

    feat: responsive etc

--- pages/ministry/etc.tsx
+++ pages/ministry/etc.tsx
@@ -9,20 +9,32 @@ const etc: NextPage = () => {
     {
       title: "NAILS ART",
       subtitile:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh malesuada viverra adipiscing mauris etiam nunc ut.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh malesuada viverra adipiscing mauris etiam nunc ut.",
+        "Workshop Nail Art diadakan pada tahun 2018 bersama Dhirani Hartono founder dari @berbienails Peserta diajarkan mengenal alat-alat untuk menghias kuku, bagaimana dasar-dasar menggunakan cat kuku, jenis-jenis cat dan hiasan kuku, tips dan juga trick dalam menghias kuku. Peserta bebas bertanya kepada pengajar tentang cara membuat Nail Art. tidak lama berselang setelah workshop, beberapa peserta berhasil membuka jasa menghias kuku.",
       imgUrl: "/assets/img/etc/etc1.png",
     },
     {
-      title: "COACHING CLINIC (FATHERING)",
+      title: "ZOOM BASIC",
       subtitile:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh malesuada viverra adipiscing mauris etiam nunc ut.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh malesuada viverra adipiscing mauris etiam nunc ut.",
-      imgUrl: "/assets/img/etc/etc2.png",
+        "Semenjak pandemi berbagai pertemuan gereja dilakukan secara online, salah satunya melalui aplikasi zoom, namun masih banyak jemaat sawangan dan sekitarnya belum fasih memakai aplikasi Zoom. inilah awal mula Zoom Basic Class di buat. Peserta diajarkan mengenal tools yang ada dan bagaimana cara menggunakannya. Mulai dari membuat gmail, mengganti profile picture, mengganti nama, mengganti background video sampai share screen. Pengajaran diberikan secara perlahan dan penuh dengan praktek agar jemaat mudah memahami materi khususnya sebagai seorang participants dalam sebuah online event.",
+      imgUrl: "/assets/img/etc/zoom.png",
     },
     {
       title: "SOCIAL MEDIA WORKSHOP",
       subtitile:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh malesuada viverra adipiscing mauris etiam nunc ut.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh malesuada viverra adipiscing mauris etiam nunc ut.",
+        "Dalam workshop ini peserta diajarkan bagaimana memproduksi konten untuk kebutuhan gereja khususnya di Instagram. Peserta diberikan edukasi dasar mengenai bagaimana algoritma instagram bekerja, bagaimana membuat sistem untuk sebuah social media team & apa saja peran yg dibutuhkan, bagaimana membuat copywriting yang baik untuk postingan instagram, tools apa saja yg dapat digunakan oleh seorang content creator, pengetahuan dasar SEO serta tips & trick lainnya.",
       imgUrl: "/assets/img/etc/etc3.png",
+    },
+    {
+      title: "ETC x DARASH DANCE",
+      subtitile:
+        "Guna memperlengkapi pelayan penari dan anak muda yang gemar menari dari segi skill, ETC berkolaborasi dengan penari profesional yang bergereja ditempat lain. Dance coach yang dipilih mengajar koreografi dengan warna yang baru bagi penari di El Roi Church. Sebagai output dari kelas tersebut, koreografi yang dipelajari telah dimodifikasi dengan kreativitas Darash Dance Crew sehingga berkembang menjadi koreo-koreo lain untuk kebutuhan ibadah mingguan sampai special performance.",
+      imgUrl: "/assets/img/etc/darashdance.png",
+    },
+    {
+      title: "MAKE UP CLASS",
+      subtitile:
+        "ETC memperlengkapi para wanita di El Roi Church yang merupakan seorang make up enthusiast, dan para pelayan jemaat khususnya penari & pelayan musik mimbar agar dapat merias diri dengan cara yang benar. ETC bertemu dengan seorang Make Up Artist profesional yang mau membagikan keahliannya. Wanita yang akrab di sapa ci Yufi (@yuficarolin.mua) secara rutin selama beberapa tahun mengajar & berkontribusi bagi El Roi Church. Melalui Make Up Workshop ini beberapa diantara peserta bukan hanya menjadi pandai merias diri, mereka juga berhasil membuka jasa rias wajah hingga saat ini.",
+      imgUrl: "/assets/img/etc/makup_etc.png",
     },
   ];
 
@@ -31,27 +43,33 @@ const etc: NextPage = () => {
       <MainLayout>
         <Banner
           title="El-Roi Training Center"
-          imgUrl="/assets/img/etc/etc.png"
+          imgUrl="/assets/img/etc/main_etc.png"
         />
-        <div className="mt-2 px-5 lg:mt-10 lg:mx-[40px] flex flex-col lg:flex-row">
-          <div className="w-full flex justify-center lg:w-1/3">
+        <div className="mt-2 px-5 lg:mt-10  lg:mx-[40px] flex flex-col lg:flex-row">
+          <div className="w-full flex justify-center lg:w-1/4">
             <img
               src="/assets/img/etc/LOGO_ETC_BARU.png"
               className="object-cover"
               alt=""
             />
           </div>
-          <div className="w-full lg:w-1/3 my-auto px-2">
+          <div className="w-full lg:w-1/2 my-auto px-2 mr-0 lg:mr-4">
             <h2 className="font-bold text-xl">WHO WE ARE</h2>
             <br />
-            <p className="font-light mb-3 lg:mb-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ornare
-              curabitur amet,massa mi nec, pellentesque.
+            <p className="font-light mb-2 lg:mb-0 text-justify indent-20">
+              El Roi Training Center merupakan sebuah wadah yang membantu jemaat
+              mengembangkan talenta yang Tuhan titipkan. El Roi Training Center
+              membangun jembatan antara keahlian dengan kesempatan dengan cara
+              memperlengkapi jemaat agar menjadi pribadi yang lebih baik. Jemaat
+              yang memiliki persiapan matang untuk menghadapi dunia, baik dari
+              segi hard skill ataupun soft skill. Harapannya ETC mampu mendorong
+              setiap potensi jemaat agar makin berkualitas. Seiring meningkatnya
+              kualitas yang dimiliki, meningkatkan pula kualitas hidup jemaat,
+              agar nama Tuhan dipermuliakan dalam segala hasil yang dikerjakan.
             </p>
           </div>
-          <div className="w-full lg:w-1/3 my-auto">
-            <h2 className="font-bold text-xl">WHO WE ARE</h2>
-            <br />
+          <div className="w-full lg:w-1/5 my-auto mt-10 lg:mt-0">
+            <h2 className="font-bold text-xl mb-2">FOLLOW US</h2>
             <button
               className="overflow-hidden mb-3 rounded-[30px] shadow-lg bg-[#F4F0F0] hover:bg-[#f0f0f0] hover:cursor-pointer py-3 px-5  sm:mr-3 text-sm text-center font-semibold my-auto flex items-center transition duration-400"
               onClick={() => {
@@ -61,6 +79,28 @@ const etc: NextPage = () => {
               <AiOutlineInstagram className="text-3xl mr-3" />
               <p className="text-xl">@etc_sawangan</p>
             </button>
+          </div>
+        </div>
+        <div className="mt-10">
+          <h2 className="font-bold mx-auto text-3xl text-center">
+            UP COMING CLASS
+          </h2>
+          <div className="my-5 lg:my-[70px]">
+            <div className="w-full xl:w-3/4 m-auto px-10 relative">
+              <div className="h-fit w-full overflow-hidden mb-3 rounded-[30px] shadow-lg">
+                <img
+                  src="/assets/img/etc/secondmain_etc.png"
+                  className="object-cover w-full"
+                  alt=""
+                />
+              </div>
+              <div className="absolute text-white left-[75px] bottom-[40px]">
+                <h2 className="font-bold text-xl md:text-3xl">DANCE CLASS</h2>
+                <p className="text-xl md:text-3xl font-thin">
+                  Senin 20 Oktober 2022
+                </p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="mt-10">
