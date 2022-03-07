0f82ece014bd06432c1b0de9b033d37f8049ca0a
keva damar galih <kevadamarg@gmail.com>
2022-03-07 14:23:06 +0700

    rev: ministry/prayer_counseling -> responsive

--- pages/ministry/prayer_counseling.tsx
+++ pages/ministry/prayer_counseling.tsx
@@ -10,10 +10,10 @@ const prayer_counseling: NextPage = () => {
       <MainLayout>
         <Banner
           title="Prayer & Counseling"
-          imgUrl="/assets/img/prayer/prayer.png"
+          imgUrl="/assets/img/prayer/newprayer.png"
         />
 
-        <div className="px-[40px] mx-auto text-center mt-[90px] w-1/2">
+        <div className="px-[40px] mx-auto text-center mt-[90px] w-full lg:w-1/2">
           <h2 className="font-bold text-3xl">
             NEED <span className="text-primary">PRAYER ?</span>
           </h2>
@@ -26,7 +26,7 @@ const prayer_counseling: NextPage = () => {
           </p>
           <br />
           <a
-            className="block uppercase sm:inline-block py-4 px-10 w-[350px] m-auto rounded-[15px] mb-4 sm:mb-0 sm:mr-3 text-lg text-black text-center font-semibold leading-none border border-primary border-4 bg-transparent hover:bg-primary hover:border-transparent hover:text-white"
+            className="block uppercase sm:inline-block py-4 lg:px-10 w-full lg:w-[350px] m-auto rounded-[15px] mb-4 sm:mb-0 sm:mr-3 text-lg text-black text-center font-semibold leading-none border border-primary border-4 bg-transparent hover:bg-primary hover:border-transparent hover:text-white"
             href="https://api.whatsapp.com/send?phone=6281210250040&text=Shalom%20ERC%20Prayer%20Call%20Center%2C%20mohon%20doakan%20saya%20mengenai%20.....%20"
             target={`_blank`}
           >
@@ -37,7 +37,7 @@ const prayer_counseling: NextPage = () => {
         </div>
 
         <div className="px-[40px] mx-auto text-center py-[60px] mt-[50px] bg-gray-100">
-          <div className="w-1/2 mx-auto">
+          <div className="w-full lg:w-1/2 mx-auto">
             <h2 className="font-bold text-3xl">
               NEED <span className="text-primary">COUNSELING ?</span>
             </h2>
@@ -105,7 +105,7 @@ const prayer_counseling: NextPage = () => {
               </div>
             </form> */}
             <a
-              className="block uppercase sm:inline-block py-4 px-10 w-[350px] m-auto rounded-[15px] mb-4 sm:mb-0 sm:mr-3 text-lg text-black text-center font-semibold leading-none border border-primary border-4 bg-transparent hover:bg-primary hover:border-transparent hover:text-white"
+              className="block uppercase sm:inline-block py-4 px-10 w-full lg:w-[350px] m-auto rounded-[15px] mb-4 sm:mb-0 sm:mr-3 text-lg text-black text-center font-semibold leading-none border border-primary border-4 bg-transparent hover:bg-primary hover:border-transparent hover:text-white"
               href="https://api.whatsapp.com/send?phone=6281210250040&text=Shalom%20ERC%20Prayer%20Call%20Center%2C%20mohon%20doakan%20saya%20mengenai%20.....%20"
               target={`_blank`}
             >
