88d83430d5c314785cd322bdc8b6827e25f39145
Antonius Joshua <Antonius Joshua>
2022-05-16 13:08:06 +0700

    [update] firebase to use .env.local

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
+import { analytics } from "../utils/firebase";
+import { useRouter } from "next/router";
+import { logEvent } from "firebase/analytics";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(process.env.NODE_ENV);
+
+    if (process.env.NODE_ENV === "production") {
+      console.log("masuk");
+      const handleRouteChange = (url: any) => {
+        logEvent(analytics, "page_view", { url });
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }
+  }, [router.events]);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
